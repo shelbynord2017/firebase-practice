1ac80dea6a25066603e8cd9be51d7e01fa05ff95
Shelbynord2017 <shelbynord2017@gmail.com>
2026-05-22 19:00:53 -0400

    logged in with avatar

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -4,19 +4,24 @@ import React from 'react'
 const Nav = ({ register, login, logout, user, loading }) => {
   return (
     <div className="nav__container">
-      <div className="App">
-        <button onClick={register} className="btn register__btn">Register</button>
-        <button onClick={login} className="btn login__btn">Login</button>
-        {loading ?
-         <>
-          <div className="skeleton__wrapper">
-            <div className="skeleton skeleton__register--btn"></div>
-            <div className="skeleton skeleton__login--btn"></div>
-          </div>
-        </>
-        :
-        user?.email 
-        } 
+      <div className="nav__row">
+        <div className="App">
+          {loading ? (
+            <div className="skeleton__wrapper">
+              <div className="skeleton skeleton__register--btn"></div>
+              <div className="skeleton skeleton__login--btn"></div>
+            </div>
+            ) : user?.email ? (
+            <div className="user__avatar">
+              {user.email[0].toUpperCase()}
+            </div>
+            ) : (
+            <>
+              <button onClick={register} className="btn register__btn">Register</button>
+              <button onClick={login} className="btn login__btn">Login</button>
+            </>
+            )}
+        </div>
       </div>
     </div>
   )
